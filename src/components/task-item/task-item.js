53dd981468301c075729936071120c9b05e0807b
slhozdemir <salihozdemir94@gmail.com>
2021-01-31 00:39:38 +0300

    task editing added

--- src/components/task-item/task-item.js
+++ src/components/task-item/task-item.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import cx from 'classnames'
 
 import { updateTask } from '../../redux'
 
 import Button from '../button'
-import { TiTick, TiEdit, TiDeleteOutline } from 'react-icons/ti'
+import { TiTick, TiDeleteOutline } from 'react-icons/ti'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsTrash } from 'react-icons/bs'
 
 import './task-item.css'
 
 const TaskItem = ({ title, order, completed, taskUrl, updateTask }) => {
+  const [isEditing, setEditing] = useState(false)
+
   const handleMark = () => {
     updateTask(taskUrl, { completed: !completed })
+  }
+
+  const handleKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      updateTitle(event)
+    } else if (event.keyCode === 27) {
+      setEditing(false)
+    }
+  }
+
+  const updateTitle = (event) => {
+    if (isEditing) {
+      const newTitle = event.target.value.trim()
+      if (title.length && title !== newTitle) {
+        updateTask(taskUrl, { title: newTitle })
+      }
+      setEditing(false)
+    }
   }
 
   return (
@@ -22,30 +44,53 @@ const TaskItem = ({ title, order, completed, taskUrl, updateTask }) => {
       >
         <TiTick />
       </Button>
-      <p
-        className={cx('task-item__title ', {
-          'task-item--completed': completed
-        })}
-      >
-        {title}
-      </p>
+      {isEditing ? (
+        <input
+          className={cx('task-item__input ', {
+            'task-item--completed': completed
+          })}
+          autoComplete="off"
+          autoFocus
+          defaultValue={title}
+          type="text"
+          maxLength="64"
+          onKeyUp={handleKeyUp}
+        />
+      ) : (
+        <p
+          className={cx('task-item__title ', {
+            'task-item--completed': completed
+          })}
+        >
+          {title}
+        </p>
+      )}
+
       <div className="task-item__actions">
-        <Button className="btn--icon btn--edit">
-          <TiEdit />
-        </Button>
-        <Button className="btn--icon btn--delete">
-          <TiDeleteOutline />
-        </Button>
+        {isEditing ? (
+          <Button
+            className="btn--icon btn--stopEdit"
+            onClick={() => setEditing(false)}
+          >
+            <TiDeleteOutline />
+          </Button>
+        ) : (
+          <>
+            <Button
+              className="btn--icon btn--edit"
+              onClick={() => setEditing(true)}
+            >
+              <AiOutlineEdit />
+            </Button>
+            <Button className="btn--icon btn--delete">
+              <BsTrash />
+            </Button>
+          </>
+        )}
       </div>
     </div>
   )
 }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     taskData: state
-//   }
-// }
 
 const mapDispatchToProps = (dispatch) => {
   return {
